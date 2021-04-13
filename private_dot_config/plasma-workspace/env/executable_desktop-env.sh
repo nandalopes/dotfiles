4115cd2d9b60ff7386a36b16f1006f96227e32c4
Nanda Lopes <nandalopes@gmail.com>
2021-04-13 11:02:30 -0300

    Capslock behavior

---
+++ private_dot_config/plasma-workspace/env/executable_desktop-env.sh
@@ -0,0 +1,9 @@
+# if [ -z "$FREEPLANE_JAVA_HOME" ]
+# then
+#   export FREEPLANE_JAVA_HOME=/home/media/bin/jre
+# fi
+#
+# [[ -z "$FREEPLANE_JAVA_HOME" ]] && export FREEPLANE_JAVA_HOME=/home/media/bin/jre
+
+# export LIBVA_DRIVER_NAME=i965
+$HOME/.xprofile

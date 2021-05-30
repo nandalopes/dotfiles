8890728c6cdb97a896343c5c04e885d2fcf66253
Nanda Lopes <nandalopes@gmail.com>
2021-05-30 09:21:32 -0300

    (kde) modify path
    (cherry picked from commit 1e1b59d7b714cb5d812761298fb630f2f25c301e)

--- private_dot_config/plasma-workspace/env/executable_desktop-env.sh
+++ private_dot_config/plasma-workspace/env/executable_desktop-env.sh
@@ -1,3 +1,12 @@
+# set PATH so it includes user's bin if it exists
+#if [ -d "$HOME/bin" ] ; then
+#  PATH="$HOME/bin:$PATH"
+#fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+  PATH="$HOME/.local/bin:$PATH"
+fi
 
 [[ -z "$LIBVA_DRIVER_NAME" ]] && export LIBVA_DRIVER_NAME=i965
 

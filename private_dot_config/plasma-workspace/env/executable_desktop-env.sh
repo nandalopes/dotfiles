b9cc20f0bd593dc8e887115aa14ec4c654f63b73
Nanda Lopes <nandalopes@gmail.com>
2021-05-30 09:20:43 -0300

    (kde) remove unused freeplane opts
    (cherry picked from commit 5ab25ee0b5c6a7b644f1ba5c9fa80f2638de9b90)

--- private_dot_config/plasma-workspace/env/executable_desktop-env.sh
+++ private_dot_config/plasma-workspace/env/executable_desktop-env.sh
@@ -1,9 +1,3 @@
-# if [ -z "$FREEPLANE_JAVA_HOME" ]
-# then
-#   export FREEPLANE_JAVA_HOME=/home/media/bin/jre
-# fi
-#
-# [[ -z "$FREEPLANE_JAVA_HOME" ]] && export FREEPLANE_JAVA_HOME=/home/media/bin/jre
 
 [[ -z "$LIBVA_DRIVER_NAME" ]] && export LIBVA_DRIVER_NAME=i965
 

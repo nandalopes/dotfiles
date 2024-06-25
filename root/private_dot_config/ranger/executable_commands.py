7c5921cb11adb98146e772b6bb9b82ad2db5add0
Nanda Lopes <nandalopes@gmail.com>
2024-06-25 17:35:32 -0300

    feat(cli): ranger-fm config
    - fasd
    - fzf_filter
    - devicons2
    - trash commands
    - etc

---
+++ root/private_dot_config/ranger/executable_commands.py
@@ -0,0 +1,50 @@
+# You can import any python module as needed.
+import os
+
+# You always need to import ranger.api.commands here to get the Command class:
+from ranger.api.commands import Command
+
+# Any class that is a subclass of "Command" will be integrated into ranger as a
+# command.  Try typing ":fasd<ENTER>" in ranger!
+class fasd(Command):
+    """
+    :fasd
+
+    Jump to directory using fasd
+    """
+    def execute(self):
+        args = self.rest(1).split()
+        if args:
+            directories = self._get_directories(*args)
+            if directories:
+                self.fm.cd(directories[0])
+            else:
+                self.fm.notify("No results from fasd", bad=True)
+
+    def tab(self, tabnum):
+        start, current = self.start(1), self.rest(1)
+        for path in self._get_directories(*current.split()):
+            yield start + path
+
+    @staticmethod
+    def _get_directories(*args):
+        import subprocess
+        output = subprocess.check_output(["fasd", "-dl"] + list(args), universal_newlines=True)
+        dirs = output.strip().split("\n")
+        dirs.sort(reverse=True)  # Listed in ascending frecency
+        return dirs
+
+class fasd_dir(Command):
+    def execute(self):
+        import subprocess
+        import os.path
+        fzf = self.fm.execute_command("fasd -dl | grep -iv cache | fzf 2>/dev/tty", universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            print(fzf_file)
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+

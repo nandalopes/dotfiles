c509755ac874cdeb7627d2c218bf38cef542a9ff
Nanda Lopes <nandalopes@gmail.com>
2025-02-20 13:38:49 -0300

    feat(cli): ranger-fm config
    - fasd
    - fzf_filter
    - devicons2
    - trash commands
    - etc

---
+++ root/private_dot_config/ranger/executable_commands.py
@@ -0,0 +1,59 @@
+from __future__ import (absolute_import, division, print_function)
+
+# You can import any python module as needed.
+import os
+
+# You always need to import ranger.api.commands here to get the Command class:
+from ranger.api.commands import Command
+
+# Any class that is a subclass of "Command" will be integrated into ranger as a
+# command.  Try typing ":fasd<ENTER>" in ranger!
+class fasd(Command):
+    """
+    :fasd
+
+    Jump to directory using fasd
+    """
+
+    # The execute method is called when you run this command in ranger.
+    def execute(self):
+        args = self.rest(1).split()
+        if args:
+            directories = self._get_directories(*args)
+            if directories:
+                self.fm.cd(directories[0])
+            else:
+                # self.fm is a ranger.core.filemanager.FileManager object and gives
+                # you access to internals of ranger.
+                # self.fm.thisfile is a ranger.container.file.File object and is a
+                # reference to the currently selected file.
+                self.fm.notify("No results from fasd", bad=True)
+
+    # The tab method is called when you press tab, and should return a list of
+    # suggestions that the user will tab through.
+    # tabnum is 1 for <TAB> and -1 for <S-TAB> by default
+    def tab(self, tabnum):
+        start, current = self.start(1), self.rest(1)
+        for path in self._get_directories(*current.split()):
+            yield start + path
+
+    @staticmethod
+    def _get_directories(*args):
+        output = subprocess.check_output(["fasd", "-dl"] + list(args), universal_newlines=True)
+        dirs = output.strip().split("\n")
+        dirs.sort(reverse=True)  # Listed in ascending frecency
+        return dirs
+
+class fasd_dir(Command):
+    def execute(self):
+        import os.path
+        fzf = self.fm.execute_command("fasd -dl | grep -iv cache | fzf 2>/dev/tty", universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            print(fzf_file)
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
